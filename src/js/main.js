13db53d6168a249abc2f780d96e1dfce5f34de41
dovigod <inaki96@naver.com>
2020-07-01 00:09:55 +0900

    finish paint-box

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,26 @@
 import { random} from "./utility";
 
-const one = random(10);
-
-const two = random(29);
 
 
+const body = document.querySelector("body");
+const header = document.querySelector(".header_flex_box");
+const head = document.querySelector("header");
 
-const init = () => {
-    console.log(`${one} ${two}`);
+const scrollTriger =() =>{
+  let scrollLocation = document.documentElement.scrollTop; // 현재 스크롤바 위치
+  if(scrollLocation>= 40)
+  {
+      header.classList.add("scroll_down");
+      head.classList.add("header_down");
+  }
+  else{
+      header.classList.remove("scroll_down");
+      head.classList.remove("header_down");
+
+  }
 }
-setInterval(init,2000);
+const init = () => {
+    window.addEventListener('scroll', scrollTriger);
+}
+
+init();
